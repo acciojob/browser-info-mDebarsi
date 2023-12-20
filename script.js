2e8771534b92b982ca0947fa34b25edf10ac2142
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-20 20:57:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ window.onload = function () {
+      // Get browser information
+      const browserName = navigator.appName;
+      const version = navigator.appVersion;
+
+      // Display the information in the browser-info div
+      const browserInfoDiv = document.getElementById('browser-info');
+      browserInfoDiv.textContent = "You are using " + browserName + " version " + version;
+    }
